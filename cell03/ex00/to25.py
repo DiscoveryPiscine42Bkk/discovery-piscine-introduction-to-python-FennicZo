2202d628fe5703ed70a101abfa7cd1b2fcb87b6b
FennicZoTH <97013152+FennicZo@users.noreply.github.com>
2025-06-24 10:58:46 +0000

    Added to25.py

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,11 @@
+print("Enter a number less than 25")
+user_inp = input()
+user_int = int(user_inp)
+
+if(user_int <= 25):
+	while user_int <= 25:
+		print(f"Inside the loop, my variable is {user_int}")
+		user_int += 1
+else:
+	print("Error")
+
