5d4946aa5f25f2cb1ad0b0c629610a50606f3f0e
FennicZoTH <97013152+FennicZo@users.noreply.github.com>
2025-06-24 08:00:33 +0000

    Added whatsyourname.py

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,4 @@
+f_name = input("Hello, may i know your first name?: ")
+l_name = input("And... what's your last name?: ")
+w_name = f_name + " " + l_name
+print(f"Alright! Good to know you, {w_name}! ")
