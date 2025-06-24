edc66e70bc432c3f47fd3597da274f558458966e
FennicZoTH <97013152+FennicZo@users.noreply.github.com>
2025-06-24 08:35:21 +0000

    Added password.py

---
+++ cell02/ex02/password.py
@@ -0,0 +1,8 @@
+password = "Protect The Pilot"
+ps_input = input("Protocol 3 : ")
+
+if(ps_input == password):
+	print("ACCESS GRANTED")
+	print("WELCOME BACK PILOT")
+else:
+	print("ACCESS DENIED")
