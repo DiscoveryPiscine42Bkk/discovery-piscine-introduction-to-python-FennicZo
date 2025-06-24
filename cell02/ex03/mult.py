5e0a969d7d4dc31804b6b2cd2d0ff55d1a7d3243
FennicZoTH <97013152+FennicZo@users.noreply.github.com>
2025-06-24 08:49:07 +0000

    Added mult.py

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,16 @@
+print("Enter the first number: ")
+f_num = input()
+f_int = int(f_num)
+print("Enter the second number: ")
+l_num = input()
+l_int = int(l_num)
+
+result = f_int * l_int
+print(f"{f_int} x {l_int} = {result}")
+
+if(result == 0):
+	print("The result is both positive and negative.")
+elif(result < 0):
+	print("The result is negative.")
+else:
+	print("The result is positive.")
